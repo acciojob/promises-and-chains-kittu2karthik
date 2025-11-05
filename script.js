0f220109d93f98475fbf5e382c73e311b8cc4efa
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-05 16:08:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ formEl.addEventListener("submit", (event) => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (age > 18) {
-        resolve(`Welcome, ${name}. You can vote.`);
+        resolve('Welcome, . You can vote.');
       } else {
-        reject(`Oh sorry, ${name}. You aren't old enough.`);
+        reject("Oh sorry . You aren't old enough.");
       }
     }, 4000);
   });
