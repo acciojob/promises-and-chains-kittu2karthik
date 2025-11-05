0b33a2510518c762b7781d6a2a478695b6bb5d07
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-05 16:07:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const formEl = document.querySelector('form');
+const ageEl = document.querySelector('#age');
+const nameEl = document.querySelector('#name');
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+  if (ageEl.value === '' || nameEl.value === '') {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  const age = Number(ageEl.value);
+  const name = nameEl.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry, ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  promise
+    .then((message) => alert(message))
+    .catch((message) => alert(message));
+});
